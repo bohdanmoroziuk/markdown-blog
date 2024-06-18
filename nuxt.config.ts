0d236fa3d2f358813411887a3e54fe86ee8eb049
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-06-18 19:45:45 +0300

    refactor: add global css via nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,15 @@ export default defineNuxtConfig({
           type: 'image/png',
           href: '/favicon.ico',
         },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+          crossorigin: ''
+        }
       ],
     },
     pageTransition: {
@@ -53,4 +62,7 @@ export default defineNuxtConfig({
       hostname: process.env.HOSTNAME,
     },
   },
+  css: [
+    '~/assets/styles/global.css',
+  ],
 })
