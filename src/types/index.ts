354bd020b89f2539838c60636ab99ec840d1c9cc
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-06-18 19:27:48 +0300

    refactor(blog): add a post type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,14 @@
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
+
 export interface Project {
   id: string
   name: string
   description: string
   html_url: string
   stargazers_count: number
+}
+
+export interface Post extends ParsedContent {
+  published_at: string
+  toc?: boolean
 }
