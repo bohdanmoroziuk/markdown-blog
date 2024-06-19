d87bf4f548b83f98dad1891029904ea8fb704be9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-06-19 18:19:02 +0300

    feat: add a copyright notice

---
+++ src/app.config.ts
@@ -0,0 +1,3 @@
+export default defineAppConfig({
+  copyrightText: 'Copyright © 2024',
+})
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,10 +2,12 @@
 useHead({
   titleTemplate: '%s - Markdown Blog',
 })
+
+const { copyrightText } = useAppConfig()
 </script>
 
 <template>
-  <div class="container mx-auto max-w-4xl px-4 mt-5">
+  <div class="container mx-auto max-w-4xl p-4 min-h-[100vh] flex flex-col gap-10">
     <header class="flex items-start md:items-center justify-between">
       <div class="flex items-center md:space-x-12">
         <AppBrand />
@@ -13,8 +15,13 @@ useHead({
       </div>
       <ColorModeSwitch />
     </header>
-    <main class="p-2 mt-10">
+    <main class="flex-1">
       <slot />
     </main>
+    <footer>
+      <p class="text-center">
+        {{ copyrightText }}
+      </p>
+    </footer>
   </div>
 </template>
